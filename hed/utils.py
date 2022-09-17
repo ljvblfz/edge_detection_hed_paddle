a2bb2d0aad2d6a710b9eae700ebc9c75adfbde48
ljvblfz <ljvblfz@users.noreply.github.com>
2022-09-17 15:30:33 +0800

    Add a new logger that support both terminal and file log

--- hed/utils.py
+++ hed/utils.py
@@ -1,9 +1,67 @@
+import logging
 import os
 
 import paddle
 
-import logger
+
+logger_initialized: dict = {}
+
+
+def get_logger(name, log_file=None, log_level=logging.INFO, file_mode='w'):
+    logger = logging.getLogger(name)
+    if name in logger_initialized:
+        return logger
+
+    # handle hierarchical names
+    # e.g., logger "a" is initialized, then logger "a.b" will skip the
+    # initialization since it is a child of "a".
+    for logger_name in logger_initialized:
+        if name.startswith(logger_name):
+            return logger
+
+    # handle duplicate logs to the console
+    # Starting in 1.8.0, PyTorch DDP attaches a StreamHandler <stderr> (NOTSET)
+    # to the root logger. As logger.propagate is True by default, this root
+    # level handler causes logging messages from rank>0 processes to
+    # unexpectedly show up on the console, creating much unwanted clutter.
+    # To fix this issue, we set the root logger's StreamHandler, if any, to log
+    # at the ERROR level.
+    for handler in logger.root.handlers:
+        if type(handler) is logging.StreamHandler:
+            handler.setLevel(logging.ERROR)
+
+    stream_handler = logging.StreamHandler()
+    handlers = [stream_handler]
+
+    if log_file is not None:
+        # Here, the default behaviour of the official logger is 'a'. Thus, we
+        # provide an interface to change the file mode to the default
+        # behaviour.
+        file_handler = logging.FileHandler(log_file, file_mode)
+        handlers.append(file_handler)
+
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    for handler in handlers:
+        handler.setFormatter(formatter)
+        handler.setLevel(log_level)
+        logger.addHandler(handler)
+
+    logger.setLevel(log_level)
+
+    logger_initialized[name] = True
+
+    return logger
+
+
+def get_root_logger(log_file=None, log_level=logging.INFO):
+    logger = get_logger(name='hed', log_file=log_file, log_level=log_level)
+    return logger
+
+
 def load_pretrained_model(model, pretrained_model):
+    logger = get_logger('Load Weight')
+
     if pretrained_model is not None:
         logger.info('Loading pretrained model from {}'.format(pretrained_model))
 
