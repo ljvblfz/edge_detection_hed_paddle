27b447ec2f48faac9d573784c597e427322b4a46
ljvblfz <ljvblfz@users.noreply.github.com>
2022-09-17 15:26:10 +0800

    Add main function wrapper

--- hed/predict.py
+++ hed/predict.py
@@ -90,6 +90,10 @@ def parse_args():
     return parser.parse_args()
 
 
-if __name__ == '__main__':
+def main():
     args = parse_args()
-    infer(args)
+	infer(args)
+
+
+if __name__ == '__main__':
+    main()
